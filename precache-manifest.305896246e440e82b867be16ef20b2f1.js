41597e290c69b42702f6853b351c921beda3ff19
VijayWilson <vijaywilsonj@gmail.com>
2021-02-19 22:57:42 +0530

    Updated code for multipart form data cors issue when uploading files

---
+++ precache-manifest.305896246e440e82b867be16ef20b2f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f97ef8162ba9aae47d8421435911208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6190655ca6e49d66d6d3",
+    "url": "/static/css/main.17aa052f.chunk.css"
+  },
+  {
+    "revision": "2e9a70591dcc38394f2c",
+    "url": "/static/js/2.bf126c47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bf126c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6190655ca6e49d66d6d3",
+    "url": "/static/js/main.93dd5aa0.chunk.js"
+  },
+  {
+    "revision": "456f41cea03430554a80",
+    "url": "/static/js/runtime-main.6770b322.js"
+  }
+]);
