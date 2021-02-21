7758c97855f3c5f99bb8d5d886657bc4e7f45749
VijayWilson <vijaywilsonj@gmail.com>
2021-02-21 23:01:14 +0530

    Updated files fixed UI issues

---
+++ precache-manifest.462e9fb53a10118eafe5cbd97bc19572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e629503bd4b2ad8a315b3ddb66c53a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c16443fdff2bf9f57600",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "875ed0ba8df9839ff183",
+    "url": "/static/js/2.272a3b18.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.272a3b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16443fdff2bf9f57600",
+    "url": "/static/js/main.335ff812.chunk.js"
+  },
+  {
+    "revision": "456f41cea03430554a80",
+    "url": "/static/js/runtime-main.6770b322.js"
+  }
+]);
