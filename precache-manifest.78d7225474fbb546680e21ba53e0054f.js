79cefa3a0198a98016fd04796c7eb2900fe6fad3
VijayWilson <vijaywilsonj@gmail.com>
2021-02-27 10:45:29 +0530

    Updated files

---
+++ precache-manifest.78d7225474fbb546680e21ba53e0054f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c90e8ea30eb5cb7f80fef9e29903f1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b207cb8a5228a43b521f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc6dc78524183cddbbf8",
+    "url": "/static/js/2.2f3c10f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2f3c10f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b207cb8a5228a43b521f",
+    "url": "/static/js/main.abb5f838.chunk.js"
+  },
+  {
+    "revision": "456f41cea03430554a80",
+    "url": "/static/js/runtime-main.6770b322.js"
+  }
+]);
